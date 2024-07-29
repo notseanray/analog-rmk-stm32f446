ff478be0593e21206d6880e411d6abca7f61201d
Sean Ray <seanray410@gmail.com>
2024-07-29 09:27:36 -0400

    scan multiplexers

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ mod keymap;
 mod vial;
 
 use crate::keymap::{COL, NUM_LAYER, ROW};
-use cortex_m::singleton;
-use embassy_stm32::{adc::{Adc, RingBufferedAdc, SampleTime, Sequence, VrefInt}, flash::{Blocking, Flash}, gpio::Pin, peripherals::USB_OTG_FS, usb::{Config as UsbConfig, Driver, InterruptHandler}, Peripherals};
+use analog_multiplexer::{DummyPin, Multiplexer};
+use embassy_stm32::{adc::{Adc, Temperature, VrefInt}, flash::{Blocking, Flash}, gpio::{Level, Output, Pin, Speed, Input}, peripherals::USB_OTG_FS, usb::{Config as UsbConfig, Driver, InterruptHandler}, Peripherals};
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::{
     bind_interrupts,
-    gpio::{AnyPin, Input, Output},
     Config,
 };
 use embassy_time::Delay;
@@ -30,13 +29,18 @@ bind_interrupts!(struct Irqs {
     OTG_FS => InterruptHandler<USB_OTG_FS>;
 });
 
+const KEYS: usize = 16 * 5;
+static mut KEYSTATES: [u16; KEYS] = [0u16; KEYS];
+static mut DEFAULT_KEYSTATES: [u16; KEYS] = [0u16; KEYS];
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
+    info!("ADC scan start!");
+    let p = embassy_stm32::init(Default::default());
+    spawner.spawn(scan_multiplexers(p)).unwrap();
     info!("RMK start!");
     // RCC config
     let config = Config::default();
-    let mut delay = Delay;
 
     // Initialize peripherals
     let p = embassy_stm32::init(config);
@@ -46,10 +50,6 @@ async fn main(_spawner: Spawner) {
     let mut usb_config = UsbConfig::default();
     usb_config.vbus_detection = true;
     let driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut EP_OUT_BUFFER.init([0; 1024])[..], usb_config);
-
-    let adc = Adc::new(p.ADC1);
-    // stabilize voltage readings
-    delay.delay_us(VrefInt::start_time_us());
 
     // Pin config
     let (input_pins, output_pins) = config_matrix_pins_stm32!(peripherals: p, input: [PD9, PD8, PB13, PB12], output: [PE13, PE14, PE15]);
@@ -64,24 +64,94 @@ async fn main(_spawner: Spawner) {
         ..Default::default()
     };
 
-    // Start serving
-    initialize_keyboard_and_run::<
-        Flash<'_, Blocking>,
-        Driver<'_, USB_OTG_FS>,
-        Input<'_>,
-        Output<'_>,
-        ROW,
-        COL,
-        NUM_LAYER,
-    >(
-        driver,
-        input_pins,
-        output_pins,
-        Some(f),
-        crate::keymap::KEYMAP,
-        keyboard_config,
-    )
-    .await;
+    unsafe {
+        // Start serving
+        initialize_keyboard_and_run::<
+            Flash<'_, Blocking>,
+            Driver<'_, USB_OTG_FS>,
+            Input<'_>,
+            Output<'_>,
+            ROW,
+            COL,
+            NUM_LAYER,
+        >(
+            driver,
+            input_pins,
+            output_pins,
+            Some(f),
+            crate::keymap::KEYMAP,
+            keyboard_config,
+            KEYSTATES,
+        )
+        .await;
+    }
+}
+
+#[embassy_executor::task]
+async fn scan_multiplexers(p: Peripherals) {
+    let mut delay = Delay;
+    let mut adc = Adc::new(p.ADC1);
+    // might not need temp, but could be used for calibration
+    // stabilize voltage readings
+    delay.delay_us(Temperature::start_time_us().max(VrefInt::start_time_us()));
+
+    let s0 = Output::new(p.PB0, Level::Low, Speed::High);
+    let s1 = Output::new(p.PB1, Level::Low, Speed::High);
+    let s2 = Output::new(p.PB2, Level::Low, Speed::High);
+    let s3 = Output::new(p.PB3, Level::Low, Speed::High);
+    // DummpyPin is enabled pin to gnd, TODO macro for this
+    let pins = (s0, s1, s2, s3, DummyPin);
+
+    let mut multiplexer = Multiplexer::new(pins);
+
+    let mut pin0 = p.PA0; // IN0
+    let mut pin1 = p.PA1; // IN1
+    let mut pin2 = p.PA2; // IN2
+    let mut pin3 = p.PA3; // IN3
+    let mut pin4 = p.PA4; // IN4
+
+    // might be doing the same thing
+    adc.set_resolution(embassy_stm32::adc::Resolution::BITS12);
+    adc.set_sample_time(embassy_stm32::adc::SampleTime::CYCLES15);
+
+    // set default values
+    for channel in 0..16 {
+        multiplexer.set_channel(channel as u8);
+        for multi in 0..5 {
+            let sample = match multi {
+                0 => adc.blocking_read(&mut pin0),
+                1 => adc.blocking_read(&mut pin1),
+                2 => adc.blocking_read(&mut pin2),
+                3 => adc.blocking_read(&mut pin3),
+                4 => adc.blocking_read(&mut pin4),
+                _ => 0,
+            };
+            unsafe {
+                DEFAULT_KEYSTATES[channel * multi] = sample;
+            }
+        }
+
+    }
+
+    loop {
+        for channel in 0..16 {
+            multiplexer.set_channel(channel as u8);
+            for multi in 0..5 {
+                let sample = match multi {
+                    0 => adc.blocking_read(&mut pin0),
+                    1 => adc.blocking_read(&mut pin1),
+                    2 => adc.blocking_read(&mut pin2),
+                    3 => adc.blocking_read(&mut pin3),
+                    4 => adc.blocking_read(&mut pin4),
+                    _ => 0,
+                };
+                unsafe {
+                    KEYSTATES[channel * multi] = sample;
+                }
+            }
+
+        }
+    }
 }
 /*
 static mut ADC1_DMA_BUF: [u16; 5] = [0u16; 5];
